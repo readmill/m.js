9e71b9009746c9d9b33f96d8b77111e850237d87
Jeff Chan <jeffchan@mit.edu>
2013-08-08 17:53:54 +0200

    Tear down modules when module element is removed from DOM

---
+++ test/specs/remove-spec.js
@@ -0,0 +1,36 @@
+describe('jQuery.event.special.remove', function () {
+
+  beforeEach(function () {
+    this.let('$element', function () {
+      return jQuery('<div>');
+    });
+
+    this.let('$child', function () {
+      return jQuery('<div>');
+    });
+
+    this.$element.append(this.$child);
+  });
+
+  it('calls the event handler on remove', function () {
+    var target = sinon.spy();
+    this.$element.on('remove', target);
+    this.$element.remove();
+    assert.called(target);
+  });
+
+  it('calls the children event handlers on remove', function () {
+    var target = sinon.spy();
+    this.$child.on('remove', target);
+    this.$element.remove();
+    assert.called(target);
+  });
+
+  it('calls the event handler when element replaced', function () {
+    var target = sinon.spy();
+    this.$child.on('remove', target);
+    this.$element.html('');
+    assert.called(target);
+  });
+
+});
